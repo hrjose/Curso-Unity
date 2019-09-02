1dcc112fcb1a89c5d7667d949aeef21fdd1a8666
Jose <hrjose_2497@hotmail.com>
2019-09-02 18:28:53 -0500

    Primeros Pasos

---
+++ Primeros pasos/Assets/Scripts/Esfera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esfera : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0,0.01f,0);
+    }
+}
---
+++ Primeros pasos/Assets/Scripts/Laberinto.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laberinto : MonoBehaviour
+{
+    private Transform tr;
+    public float velocidad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float z = Input.GetAxis("Horizontal")*velocidad*Time.deltaTime;
+
+        float x = Input.GetAxis("Vertical")*velocidad*-1*Time.deltaTime;
+
+        tr.Rotate(new Vector3(x,0,z));
+
+
+        
+    }
+}
